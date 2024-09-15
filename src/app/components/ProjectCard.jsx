c2147be1339e01501c4750d47d684af7f58c484c
Ali Mohamed <110411510+alim-99@users.noreply.github.com>
2024-09-15 16:43:34 +0300

    Update ProjectCard.jsx

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -6,9 +6,7 @@ import Image from "next/image";
 const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
   return (
     <div>
-      <div
-        className="h-52 md:h-72 rounded-t-xl relative group"
-      >
+      <div className="h-52 md:h-72 rounded-t-xl relative group">
         <Image
           src={imgUrl}
           alt={title}
@@ -16,7 +14,7 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
           objectFit="cover"
           className="rounded-t-xl"
         />
-        <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
+        <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500">
           <Link
             href={gitUrl}
             className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
@@ -31,7 +29,7 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
           </Link>
         </div>
       </div>
-      <div className="text-white rounded-b-xl mt-3 bg-[#181818]py-6 px-4">
+      <div className="text-white rounded-b-xl mt-3 bg-[#181818] py-6 px-4">
         <h5 className="text-xl font-semibold mb-2">{title}</h5>
         <p className="text-[#ADB7BE]">{description}</p>
       </div>
@@ -39,4 +37,4 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
   );
 };
 
-export default ProjectCard;
+export default ProjectCard
