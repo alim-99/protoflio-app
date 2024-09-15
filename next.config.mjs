f33cf3d441888ac861ebee37be638431e3fd1d28
Ali Mohamed <110411510+alim-99@users.noreply.github.com>
2024-09-15 16:22:05 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+      domains: ['localhost', 'https://protoflio-app.vercel.app/'],
+    },
+};
 
 export default nextConfig;
