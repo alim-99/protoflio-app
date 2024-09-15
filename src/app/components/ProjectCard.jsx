1a54370d6878b79fe03db4de729b5c8e45ff07f6
Ali Mohamed <110411510+alim-99@users.noreply.github.com>
2024-09-15 16:49:30 +0300

    Update ProjectCard.jsx

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -1,20 +1,15 @@
 import React from "react";
 import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
-import Image from "next/image";
 
 const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
   return (
     <div>
-      <div className="h-52 md:h-72 rounded-t-xl relative group">
-        <Image
-          src={imgUrl}
-          alt={title}
-          layout="fill"
-          objectFit="cover"
-          className="rounded-t-xl"
-        />
-        <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500">
+      <div
+        className="h-52 md:h-72 rounded-t-xl relative group"
+        style={{ background: imgUrl, backgroundSize: "cover" }}
+      >
+        <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
           <Link
             href={gitUrl}
             className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
@@ -29,7 +24,7 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
           </Link>
         </div>
       </div>
-      <div className="text-white rounded-b-xl mt-3 bg-[#181818] py-6 px-4">
+      <div className="text-white rounded-b-xl mt-3 bg-[#181818]py-6 px-4">
         <h5 className="text-xl font-semibold mb-2">{title}</h5>
         <p className="text-[#ADB7BE]">{description}</p>
       </div>
@@ -37,4 +32,4 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
   );
 };
 
-export default ProjectCard
+export default ProjectCard;
