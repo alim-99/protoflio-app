43252caf2ded5e3877b79552ff669c35b8f90581
Ali Mohamed <110411510+alim-99@users.noreply.github.com>
2024-09-15 16:30:02 +0300

    Update ProjectCard.jsx

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { CodeBracketIcon, EyeIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
+import Image from "next/image";
 
 const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
   return (
     <div>
       <div
         className="h-52 md:h-72 rounded-t-xl relative group"
-        style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
       >
+        <Image
+          src={imgUrl}
+          alt={title}
+          layout="fill"
+          objectFit="cover"
+          className="rounded-t-xl"
+        />
         <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
           <Link
             href={gitUrl}
